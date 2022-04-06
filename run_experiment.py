9dc97f1e56d5cb0a568e2fe464c887caf9f056ff
Michael Ogezi <okibeogezi@outlook.com>
2022-04-06 03:43:41 +0000

    chore: ran full eval on gold and full silver

--- run_experiment.py
+++ run_experiment.py
@@ -3,6 +3,8 @@ import os
 import argparse
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
+import numpy as np
+import torch
 
 '''
     python run_experiment.py
@@ -12,6 +14,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--output_file", "-o", default='./ranked_sample_synsets.json')
 parser.add_argument("--input_file", "-i", default='./sample_synsets.json')
 parser.add_argument("--model", "-m", default='openai/clip-vit-base-patch32', choices=['openai/clip-vit-base-patch32', 'openai/clip-vit-base-patch16', 'openai/clip-vit-large-patch14'])
+parser.add_argument("--image_batch_size", "-bs", default=100, type=int)
+parser.add_argument("--device", "-d", default='cuda', type=str)
 
 args = parser.parse_args()
 max_seq_len = 77
@@ -19,7 +23,7 @@ max_seq_len = 77
 _in = json.load(open(args.input_file, 'r'))
 out_dict = json.load(open(args.input_file, 'r'))
 
-device = 'cuda'
+device = args.device
 model = CLIPModel.from_pretrained(args.model).to(device)
 processor = CLIPProcessor.from_pretrained(args.model)
 pred_map = {
@@ -28,30 +32,45 @@ pred_map = {
     2: 'example',
 }
 
-for id, v in _in.items():
-    images_objs = v['images']
-    
-    images = [Image.open(i['path']).resize((224, 224)).convert('RGB') for i in images_objs]
-    texts = [
-        ', '.join(v['lemmas'])[:], 
-        v['gloss'][:] if ('gloss' in v and v['gloss']) else None,
-        v['example'][:] if ('example' in v and v['example']) in v else None,
-    ]
-    text_pops = [bool(text) for text in texts]
+with torch.no_grad():
+    progress = 0
+    for id, v in _in.items():
+        progress += 1
+        images_objs = v['images']
 
-    inputs = processor(text=[text for text in texts if bool(text)], images=images, return_tensors="pt", padding=True, truncation=True)
-    inputs = inputs.to(device)
-    outputs = model(**inputs)
-    
-    probs = outputs.logits_per_image.softmax(dim=0)
-    # print(probs)
+        images = [Image.open(i['path']).resize((224, 224)).convert('RGB') for i in images_objs]
+        print('Processing {: 4} images for {}/{}\r'.format(len(images), progress, len(_in)), end='')
 
-    for r_idx, r in enumerate(probs):
-        c_idx = 0
-        for idx in range(len(texts)):
-            populated = text_pops[idx]
-            out_dict[id]['images'][r_idx]['{}_confidence'.format(pred_map[idx])] = float(r[c_idx]) if populated else None
-            if populated:
-                c_idx += 1
+        texts = [
+            ', '.join(v['lemmas'])[:],
+            v['gloss'][:] if ('gloss' in v and v['gloss']) else None,
+            v['example'][:] if ('example' in v and v['example']) in v else None,
+        ]
+        text_pops = [bool(text) for text in texts]
+
+        probs = torch.Tensor().to(device)
+        start = 0
+        end = args.image_batch_size
+        will_break = False
+        while True:
+            if will_break:
+                break
+            __images = images[start:end]
+            start += args.image_batch_size
+            end += args.image_batch_size
+            will_break = (len(images) - end) <= 0
+            inputs = processor(text=[text for text in texts if bool(text)], images=__images, return_tensors="pt", padding=True, truncation=True)
+            inputs = inputs.to(device)
+            outputs = model(**inputs)
+            __probs = outputs.logits_per_image.softmax(dim=0)
+            probs = torch.cat((probs, __probs), dim=0)
+        
+        for r_idx, r in enumerate(probs):
+            c_idx = 0
+            for idx in range(len(texts)):
+                populated = text_pops[idx]
+                out_dict[id]['images'][r_idx]['{}_confidence'.format(pred_map[idx])] = float(r[c_idx]) if populated else None
+                if populated:
+                    c_idx += 1
 
 json.dump(out_dict, open(args.output_file, 'w'), ensure_ascii=False, indent=2)
